4f22218cfe273439e1f3207acf9da1acbd4363ea
Bruh <skibididotporn@gmail.com>
2024-10-16 15:29:52 +0300

    Add files via upload
    ну типа новое добавили там настройки и тп. Чмэк одним словом

---
+++ ApplicationSettings Фамилия/ApplicationSettings Фамилия/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ApplicationSettings_Фамилия
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public enum pages
+        {
+            setting
+        }
+
+        public void OpenPage(pages _pages)
+        {
+            if (_pages == pages.setting)
+                Frame.Navigate(new Pages.Settings(this));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            OpenPage(pages.setting);
+        }
+    }
+}
